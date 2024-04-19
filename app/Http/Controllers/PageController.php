d16d20eeb166045a54cdb46baa8324780fd1a08e
Stevaniputri <stevanipt@gmail.com>
2024-04-20 04:22:21 +0700

    update

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -19,53 +19,8 @@ class PageController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function register(Request $request)
-    {
-        $request->validate([
-            'fullname' => 'required',
-            'username' => 'required',
-            'email' => 'required',
-            'address' => 'required',
-            'password' => 'required',
-        ]);
-
-        User::create([
-            'fullname' => $request->fullname,
-            'username' => $request->username,
-            'email' => $request->email,
-            'address' => $request->address,
-            'password' => Hash::make($request->password),
-            'role' => 'peminjam'
-        ]);
-
-        return redirect()->route('authPage')->with('success', 'berhasil membuat akun');
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
-    public function auth(Request $request)
-    {
-        $request->validate([
-            'username' => 'required|exists:user,username',
-            'password' => 'required'
-        ]);
-
-        $user = $request->only('username', 'password');
-        
-        if(Auth::attempt($user)){
-            $role = Auth::user()->role;
-            if($role === 'admin' || $role === 'petugas'){
-                return redirect()->route('dashboard');
-            }else {
-                return redirect()->route('dashboardUser');
-            }
-        }else {
-            return redirect('/');
-        }
-    }
 
     /**
      * Display the specified resource.
